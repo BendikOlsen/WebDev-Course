c3535667a22ccae31bcf306c8f3b4c5b02021b04
bendikolsen <bendikolsen@gmail.com>
2018-10-10 15:31:07 +0200

    authentication demo app

---
+++ JS/Authentication/AuthDemo/app.js
@@ -0,0 +1,60 @@
+var     express                 = require("express"),
+        mongoose                = require("mongoose"),
+        passport                = require("passport"),
+        bodyParser              = require("body-parser"),
+        User                    = require("./models/user"),
+        localStrategy           = require("passport-local"),
+        passportLocalMongoose   = require("passport-local-mongoose")
+mongoose.connect("mongodb://localhost/auth_demo_app", { useNewUrlParser: true });
+
+var app     = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "Hemmelig passord",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//==========
+//  ROUTES
+//==========
+
+app.get("/", function(req, res){
+    res.render("home");
+})
+
+app.get("/secret", function(req, res){
+    res.render("secret")
+})
+
+// Auth Routes
+
+//show sign up form
+app.get("/register", function(req, res){
+    res.render("register")
+})
+//handling user sign up
+app.post("/register", function(req, res){
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err)
+            return res.render('register')
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret")
+        })
+    })
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server live!")
+})
