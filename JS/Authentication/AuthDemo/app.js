5c7cf3b9bbb18f8481a715babb845ca1520db70d
bendikolsen <bendikolsen@gmail.com>
2018-10-12 12:47:37 +0200

    simple authentication demo

--- JS/Authentication/AuthDemo/app.js
+++ JS/Authentication/AuthDemo/app.js
@@ -3,7 +3,7 @@ var     express                 = require("express"),
         passport                = require("passport"),
         bodyParser              = require("body-parser"),
         User                    = require("./models/user"),
-        localStrategy           = require("passport-local"),
+        LocalStrategy           = require("passport-local"),
         passportLocalMongoose   = require("passport-local-mongoose")
 mongoose.connect("mongodb://localhost/auth_demo_app", { useNewUrlParser: true });
 
@@ -19,6 +19,7 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -30,7 +31,7 @@ app.get("/", function(req, res){
     res.render("home");
 })
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
     res.render("secret")
 })
 
@@ -54,6 +55,31 @@ app.post("/register", function(req, res){
         })
     })
 })
+
+// LOGIN ROUTES
+//render login form
+app.get("/login", function(req, res){
+    res.render("login")
+})
+//login logic
+//middleware
+app.post("/login",passport.authenticate("local",{
+    successRedirect:"/secret",
+    failureRedirect:"/login"
+}) , function(req, res){
+})
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+})
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server live!")
